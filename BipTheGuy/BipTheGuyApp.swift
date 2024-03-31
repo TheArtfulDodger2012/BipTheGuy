8795603bf3b40ff0254a8ea0dff158ae84899f47
Ron Lane <theartfuldodger2012@gmail.com>
2024-03-31 19:02:40 -0400

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Ron Lane on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
